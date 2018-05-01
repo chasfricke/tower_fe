813d17ae79fd4d28c77271b79fc3414d9d85ad50
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-04-30 18:05:30 -0600

    delete working

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -6,15 +6,33 @@ export class LocationList extends Component {
         this.state = {}
     }
 
+    deleteLocation = (location) => {
+        if (window.confirm("Delete location: " + location.name + " @ " + location.address)){
+            return fetch('https://tower-be.herokuapp.com/locations/' + location.id, {
+                method: 'DELETE',
+                headers: new Headers({
+                'Content-Type': 'application/json'
+                })
+            }).catch(error => console.error('Error', error))
+        } 
+    }
+
+    updateLocation = (location) => {
+        console.log("update clicked " + location.name + location.id)
+    }
+
     render () {
         return (
             <ul className="locations-list">
                 {this.props.locations.map(location => {
                         return (
-                            <li>
+                            <li className="location-card">
                                 <h4>{location.name}</h4>
+                                <p>ID: {location.id}</p>
                                 <p>{location.business_type}</p>
-                                <p>{location.address}</p>                          
+                                <p>{location.address}</p>
+                                <button onClick={() => this.deleteLocation(location)}>Delete</button>     
+                                <button onClick={() => this.updateLocation(location)}>Update</button>                     
                             </li>
                         )
                     })}
